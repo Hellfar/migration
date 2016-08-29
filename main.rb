130bbff84c9163bbe8ae787cca71083dc208ccc5
pierre.l <pierre.l@snapkin.fr>
2016-08-29 18:20:20 +0200

    dependant tables.

--- main.rb
+++ main.rb
@@ -72,6 +72,10 @@ if __FILE__ == $0
   puts "    create_table :#{table[:name].pluralize} do |t|"
   puts
   puts "      t.timestamps null: false"
+    @tables.select{|t|t[:type]==:reference&&t[:connections][1]==table[:id]}.each do | reference |
+      depend = @tables.select{|t|t[:id]==reference[:connections][0]}.first[:name]
+  puts "      t.reference #{depend}, null: true, foreign_key: true, type: :uuid"
+    end
     table[:fields].each do | field |
   puts "      t.#{field[:type]} :#{field[:name]}"
     end
